6c5874ca34e6e83ae0de382475b37e3d3360c0c9
Adil <christie_m@mail.ru>
2024-05-10 17:23:57 +0300

    Android_weatherapp

---
+++ WeatherApp/app/src/main/java/dev/m13d/weatherapp/MainActivity.java
@@ -0,0 +1,24 @@
+package dev.m13d.weatherapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int countOfFragmentInManager = getSupportFragmentManager().getBackStackEntryCount();
+        if(countOfFragmentInManager > 0) {
+            getSupportFragmentManager().popBackStack();
+            getSupportFragmentManager().popBackStack("Some_Key", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+}
